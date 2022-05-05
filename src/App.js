e69ba59928a26e64bdbb908ffe65e97f5097b13b
Mikgamer <michaelperard@gmail.com>
2022-05-05 11:25:33 +0200

    feat : loader for form and optimization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom"
+import React, { Suspense } from 'react'
 
-import FormEmployee from "./components/FormEmployee"
+import { Link } from "react-router-dom"
+const FormEmployee = React.lazy(() => import("./components/FormEmployee"))
+// import FormEmployee from "./components/FormEmployee"
 
 const App = () => {
   return (
     <main className="container">
       <h1>HRnet</h1>
       <Link to="employees" className="link">View Current Employees</Link>
-      <FormEmployee />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormEmployee />
+      </Suspense>
     </main>
   )
 }
